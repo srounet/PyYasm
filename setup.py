dab96d8238f7dba6ce19fa07f023bf81a9d26d99
Fabien Reboia <srounet@gmail.com>
2017-05-07 11:06:07 +0200

    fix setup use rst

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 here = os.path.abspath(os.path.dirname(__file__))
 
 # Get the long description from the relevant file
-with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 
